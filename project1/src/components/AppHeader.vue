4dcdd65e6c0e4b38cc75308c87445366543a1339
Hoàng Thu Hà <58552143+hoangha-fedev@users.noreply.github.com>
2020-01-08 18:01:15 +0700

    add AppHeader

---
+++ project1/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+
+  data: () => ({
+    
+  }),
+};
+</script>
