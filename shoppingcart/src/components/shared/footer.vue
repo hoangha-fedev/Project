fda21604de717dfa56ce615afe826b2df04c27bf
hoangha-fedev <hoangha.fedev@gmail.com>
2020-01-16 12:09:50 +0700

    shoppingcard finish

---
+++ shoppingcart/src/components/shared/footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-footer dark height="auto" class="pa-0">
+      <v-card flat tile class="indigo lighten-1 white--text text-center flex">
+        <v-card-text>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
+            <v-icon size="24px">{{icon}}</v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text>Shopping card footer text here</v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text text-center">
+          &copy;2019 -
+          <strong>Shoping Card</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: [
+      "mdi-facebook",
+      "mdi-twitter",
+      "mdi-google-plus",
+      "mdi-linkedin",
+      "mdi-instagram"
+    ]
+  })
+}
+</script>
---
+++ shoppingcart/src/components/shared/navbar.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Menu</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" :href="item.href">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Shopping Cart</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn text>Products</v-btn>
+        <v-btn text>
+          <v-badge left color="green">
+            <span slot="badge">5</span>
+            <v-icon>mdi-cart</v-icon>
+          </v-badge>Basket
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+    items: [
+      {
+        icon: "mdi-account",
+        href: "#",
+        title: "Account"
+      }
+    ],
+    drawer: false
+  })
+}
+</script>
