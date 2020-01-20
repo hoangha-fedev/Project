f609a2bb3db7e946693d32174ed6be541d42dbf7
hoangha-fedev <hoangha.fedev@gmail.com>
2020-01-20 17:31:16 +0700

    Profile Card

--- vuetify-project/src/views/Profile.vue
+++ vuetify-project/src/views/Profile.vue
@@ -1,15 +1,42 @@
 <template>
-    <div>
-        <h1>Profile</h1>
-    </div>
+  <div>
+    <v-card max-width="344" class="mx-auto mt-7 mb-5">
+      <v-list-item>
+        <v-list-item-avatar>
+            <v-img src="https://w0.pngwave.com/png/129/94/computer-icons-avatar-icon-design-male-teacher-png-clip-art-thumbnail.png"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="headline">Hoàng Thu Hà</v-list-item-title>
+          <v-list-item-subtitle>Front-End Developer</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+
+      <v-card-text>
+          <p>Email: hoangha.fedev@gmail.com</p>
+          <p>Tel: +84974653402</p>
+          <p>Skype: ha.hoang@savvycomsoftware.com</p>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn text color="deep-purple accent-4">Read</v-btn>
+        <v-btn text color="deep-purple accent-4">Bookmark</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-share-variant</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style lang="scss">
-
 </style>
